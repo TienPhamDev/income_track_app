4bba02403c14728d11f2415984ff144ff0135332
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-04-28 04:29:03 +0000

    code connect to database

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express")
+const Datastore = require("nedb")
 const app = express()
 app.listen(3000, () => {
   console.log("listening 3000")
 })
+
+const dataBase = new Datastore("database.db")
+dataBase.loadDatabase()
+
 app.use(express.static("public"))
 app.use(express.json({limit :"1mb"}))
 
 app.post("/api", (request, response) => {
   console.log(request.body)
-
+  const data = request.body
+  dataBase.insert(data)
   response.json({
     status:"success"
   })
