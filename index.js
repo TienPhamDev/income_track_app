ca8d57807a479e4609ceb5b26013a7262a4cb637
TienPhamDev <manhtienbn18891@gmail.com>
2024-04-27 23:47:20 -0400

    code

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.listen(3000, () => {
 app.use(express.static("public"))
 
 app.post("/api", (request, response) => {
-  console.log(request)
+  console.log(request.body)
+
+  response.json({
+    status:"success"
+  })
 })
