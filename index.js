52041128f36b121db4eeb51e254e3bce7c719cea
TienPhamDev <manhtienbn18891@gmail.com>
2024-04-27 23:55:01 -0400

    node

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ app.listen(3000, () => {
   console.log("listening 3000")
 })
 app.use(express.static("public"))
+app.use(express.json({limit :"1mb"}))
 
 app.post("/api", (request, response) => {
   console.log(request.body)
