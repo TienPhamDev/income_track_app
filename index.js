a2436ee12ba93430b40ff2247f79891b73ad1b80
TienPhamDev <manhtienbn18891@gmail.com>
2024-04-29 22:00:33 -0400

    code

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const Datastore = require("nedb")
 const app = express()
+const path = require("path")
+
+app.set("view engine", "ejs")// ejs is a template engine
+app.set("views", path.join(__dirname), "")
+
+app.use("/assets", express.static("assets"))
+
 app.listen(3000, () => {
   console.log("listening 3000")
 })
@@ -8,9 +15,13 @@ app.listen(3000, () => {
 const dataBase = new Datastore("database.db")
 dataBase.loadDatabase()
 
-app.use(express.static("public"))
+// app.use(express.static("public"))
 app.use(express.json({limit :"1mb"}))
 
+
+app.get("/", (req, res) => {
+  res.render("index",{})
+})
 app.get("/api", (request, response) => {
   dataBase.find({}, (err, data) => {
     response.json(data)
